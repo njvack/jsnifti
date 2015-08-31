f31b49479036c6ed7e0bc0450c5f765da6180fee
Nate Vack <njvack@wisc.edu>
2015-08-31 13:52:47 -0500

    Quick example code. Doesn't do much.

---
+++ example/ndar.js
@@ -0,0 +1,5 @@
+window.ndarray = require('ndarray');
+//window.save_pixels = require('save-pixels');
+window.ops = require('ndarray-ops');
+window.show = require('ndarray-show');
+window.imshow = require ('ndarray-imshow');
