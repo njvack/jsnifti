581ce18d4343c2d01cf6173844a613a234e05ef8
Nate Vack <njvack@wisc.edu>
2015-09-02 11:33:44 -0500

    Some minor updates

--- example/ndar.js
+++ example/ndar.js
@@ -1,5 +1,10 @@
+// The bulk of the magic
+window.nifti1Image = require('./nifti');
+// An easy way to drop these pixels on screen
+window.save_pixels = require('save-pixels');
+
+// These are all for console debugging, uncomment for fun and happiness
 window.ndarray = require('ndarray');
-//window.save_pixels = require('save-pixels');
 window.ops = require('ndarray-ops');
 window.show = require('ndarray-show');
-window.imshow = require ('ndarray-imshow');
+window.imshow = require('ndarray-imshow');
